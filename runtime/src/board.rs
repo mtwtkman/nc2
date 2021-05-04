f6e2cb92dc96e54161e66898ae61a0b6c73eaa7c
mtwtkman <punipuniomochi@gmail.com>
2021-05-04 22:14:49 +0900

    added board initializer

--- runtime/src/board.rs
+++ runtime/src/board.rs
@@ -6,8 +6,9 @@ use crate::{
     position::{Column, Position, Row},
 };
 
-type CellMap = HashMap<Cell, MovingRange>;
+type CellMap = HashMap<Position, Cell>;
 
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) struct Board {
     cell_map: CellMap,
 }
@@ -19,7 +20,12 @@ impl Board {
     }
 
     fn setup(player_a: &Player, player_b: &Player) -> CellMap {
-        let player_a_side_cells = Self::build_initial_occupied_row(player_a, Row::Top);
+        let mut cell_map = CellMap::new();
+        let player_a_side_cells = Self::generate_initial_occupied_cells(player_a.clone(), Row::Top);
+        player_a_side_cells
+            .for_each(|(position, cell)| {
+                cell_map.insert(position, cell);
+            });
         let empty_rows = [
             Row::MiddleFirst,
             Row::MiddleSecond,
@@ -27,30 +33,20 @@ impl Board {
             Row::MiddleFourth,
         ]
         .iter()
-        .map(|row| Self::build_initial_empty_row(row))
-        .collect::<Vec<Vec<Cell>>>();
-        let player_b_side_cells = Self::build_initial_occupied_row(player_b, Row::Bottom);
-        unimplemented!()
+        .flat_map(|row| Self::generate_initial_empty_cells(row.to_owned()));
+        empty_rows
+            .for_each(|(position, cell)| {
+                cell_map.insert(position, cell);
+            });
+        let player_b_side_cells = Self::generate_initial_occupied_cells(player_b.clone(), Row::Bottom);
+        player_b_side_cells
+            .for_each(|(position, cell)| {
+                cell_map.insert(position, cell);
+            });
+        cell_map
     }
 
-    fn build_initial_occupied_row(player: &Player, side: Row) -> Vec<Cell> {
-        let columns = [
-            Column::LeftEdge,
-            Column::MiddleFirst,
-            Column::MiddleSecond,
-            Column::MiddleThird,
-            Column::RightEdge,
-        ];
-        columns
-            .iter()
-            .map(|column| {
-                let position = Position::new(column.to_owned(), side.clone());
-                Cell::new_occupied(&position, player)
-            })
-            .collect::<Vec<Cell>>()
-    }
-
-    fn build_initial_empty_row(row: &Row) -> Vec<Cell> {
+    fn generate_initial_occupied_cells(player: Player, side: Row) -> impl Iterator<Item=(Position, Cell)> {
         [
             Column::LeftEdge,
             Column::MiddleFirst,
@@ -59,11 +55,27 @@ impl Board {
             Column::RightEdge,
         ]
         .iter()
-        .map(|column| {
-            let position = Position::new(column.to_owned(), row.clone());
-            Cell::new_empty(&position)
+        .map(move |column| {
+            let position = Position::new(column.to_owned(), side.clone());
+            let cell = Cell::new_occupied(player.clone());
+            (position,cell)
         })
-        .collect::<Vec<Cell>>()
+    }
+
+    fn generate_initial_empty_cells(row: Row) -> impl Iterator<Item=(Position, Cell)> {
+        [
+            Column::LeftEdge,
+            Column::MiddleFirst,
+            Column::MiddleSecond,
+            Column::MiddleThird,
+            Column::RightEdge,
+        ]
+        .iter()
+        .map(move |column| {
+            let position = Position::new(column.to_owned(), row.clone());
+            let cell = Cell::new_empty();
+            (position, cell)
+        })
     }
 }
 
@@ -77,15 +89,35 @@ struct MovingRange {
     down_right: Option<Cell>,
     up_left: Option<Cell>,
     down_left: Option<Cell>,
+    pivot: Cell,
+}
+
+impl MovingRange {
+    fn default(pivot: Cell) -> Self {
+        Self {
+            up: None,
+            down: None,
+            right: None,
+            left: None,
+            up_right: None,
+            down_right: None,
+            up_left: None,
+            down_left: None,
+            pivot,
+        }
+    }
 }
 
 #[test]
-fn build_initial_occupied_row() {
-    use crate::{cell::Pallet, position::Column};
+fn generate_initial_occupied_cells() {
+    use crate::{
+        cell::Cell,
+        position::Column,
+    };
 
     for side in [Row::Top, Row::Bottom].iter() {
         let player = Player::new();
-        let side_row = Board::build_initial_occupied_row(&player, side.to_owned());
+        let side_row = Board::generate_initial_occupied_cells(player.clone(), side.to_owned()).collect::<Vec<(Position, Cell)>>();
         let expected_cells = [
             Column::LeftEdge,
             Column::MiddleFirst,
@@ -94,23 +126,19 @@ fn build_initial_occupied_row() {
             Column::RightEdge,
         ]
         .iter()
-        .map(|column| Cell {
-            position: Position::new(column.to_owned(), side.to_owned()),
-            pallet: Pallet {
-                players: [Some(player.clone()), None, None],
-            },
+        .map(|column| {
+            let position = Position::new(column.to_owned(), side.to_owned());
+            let cell = Cell::new_occupied(player.clone());
+            (position, cell)
         })
-        .collect::<Vec<Cell>>();
+        .collect::<Vec<(Position, Cell)>>();
         assert_eq!(side_row, expected_cells);
     }
 }
 
 #[test]
-fn build_initial_empty_row() {
-    use crate::{
-        cell::{Pallet, PALLET_HEIGHT_LIMIT},
-        position::Column,
-    };
+fn generate_initial_empty_cells() {
+    use crate::position::Column;
 
     for row in [
         Row::MiddleFirst,
@@ -128,14 +156,63 @@ fn build_initial_empty_row() {
             Column::RightEdge,
         ]
         .iter()
-        .map(|column| Cell {
-            position: Position::new(column.to_owned(), row.to_owned()),
-            pallet: Pallet {
-                players: [None; PALLET_HEIGHT_LIMIT],
-            },
+        .map(move |column| {
+            let position = Position::new(column.to_owned(), row.to_owned());
+            let cell = Cell::new_empty();
+            (position, cell)
         })
-        .collect::<Vec<Cell>>();
-        let row = Board::build_initial_empty_row(row);
+        .collect::<Vec<(Position, Cell)>>();
+        let row = Board::generate_initial_empty_cells(row.clone()).collect::<Vec<(Position, Cell)>>();
         assert_eq!(row, expected_cells);
     }
+}
+
+#[test]
+fn new() {
+    let player_a = Player::new();
+    let player_b = Player::new();
+    let board = Board::new(&player_a, &player_b);
+    let mut expected: CellMap = CellMap::new();
+    let columns =
+    [
+        Column::LeftEdge,
+        Column::MiddleFirst,
+        Column::MiddleSecond,
+        Column::MiddleThird,
+        Column::RightEdge,
+    ];
+    columns
+        .iter()
+        .for_each(|column| {
+            expected.insert(
+                Position::new(column.to_owned(), Row::Top),
+                Cell::new_occupied(player_a.clone()),
+            );
+        });
+    [
+        Row::MiddleFirst,
+        Row::MiddleSecond,
+        Row::MiddleThird,
+        Row::MiddleFourth,
+    ]
+    .iter()
+    .for_each(|row| {
+        columns
+            .iter()
+            .for_each(|column| {
+                expected.insert(
+                    Position::new(column.to_owned(), row.to_owned()),
+                    Cell::new_empty(),
+                );
+            });
+    });
+    columns
+        .iter()
+        .for_each(|column| {
+            expected.insert(
+                Position::new(column.to_owned(), Row::Bottom),
+                Cell::new_occupied(player_b.clone()),
+            );
+        });
+    assert_eq!(board, Board { cell_map: expected });
 }
--- runtime/src/cell.rs
+++ runtime/src/cell.rs
@@ -1,24 +1,23 @@
 use crate::player::Player;
-use crate::position::Position;
 use crate::result::{Error, Result};
 
 pub(crate) const PALLET_HEIGHT_LIMIT: usize = 3;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
-pub(crate) struct Pallet {
-    pub(crate) players: [Option<Player>; PALLET_HEIGHT_LIMIT],
+pub(crate) struct Cell {
+    pub(crate) pallet: [Option<Player>; PALLET_HEIGHT_LIMIT],
 }
 
-impl Pallet {
+impl Cell {
     fn height(&self) -> usize {
-        self.players.iter().filter(|x| x.is_some()).count()
+        self.pallet.iter().filter(|x| x.is_some()).count()
     }
 
-    fn apex(&self) -> Option<Player> {
+    fn owner(&self) -> Option<Player> {
         if self.is_empty() {
             None
         } else {
-            self.players[self.height() - 1].clone()
+            self.pallet[self.height() - 1].clone()
         }
     }
 
@@ -26,15 +25,15 @@ impl Pallet {
         self.height() == 0
     }
 
-    fn empty() -> Self {
+    pub(crate) fn new_empty() -> Self {
         Self {
-            players: [None; PALLET_HEIGHT_LIMIT],
+            pallet: [None; PALLET_HEIGHT_LIMIT],
         }
     }
 
-    fn occupied(player: &Player) -> Self {
+    pub(crate) fn new_occupied(player: Player) -> Self {
         Self {
-            players: [Some(player.clone()), None, None],
+            pallet: [Some(player), None, None],
         }
     }
 
@@ -43,13 +42,13 @@ impl Pallet {
         if height == PALLET_HEIGHT_LIMIT {
             Err(Error::ReachedPalletHeightLimit)
         } else {
-            let apex = self.apex();
-            if apex == Some(player.clone()) {
+            let owner = self.owner();
+            if owner == Some(player.clone()) {
                 Err(Error::AlreadyOccupied(player.clone()))
             } else {
-                let mut players = self.players.clone();
+                let mut players = self.pallet.clone();
                 players[height] = Some(player.clone());
-                Ok(Self { players })
+                Ok(Self { pallet: players })
             }
         }
     }
@@ -58,105 +57,39 @@ impl Pallet {
         if self.is_empty() {
             Err(Error::PalletIsEmpty)
         } else {
-            let mut players = self.players.clone();
+            let mut players = self.pallet.clone();
             players[self.height() - 1] = None;
-            Ok(Self { players })
+            Ok(Self { pallet: players })
         }
-    }
-}
-
-#[derive(Debug, Eq, PartialEq, Clone)]
-pub(crate) struct Cell {
-    pub(crate) position: Position,
-    pub(crate) pallet: Pallet,
-}
-impl Cell {
-    pub(crate) fn new_empty(position: &Position) -> Self {
-        Self {
-            position: position.clone(),
-            pallet: Pallet::empty(),
-        }
-    }
-
-    pub(crate) fn new_occupied(position: &Position, player: &Player) -> Self {
-        Self {
-            position: position.clone(),
-            pallet: Pallet::occupied(player),
-        }
-    }
-
-    pub(crate) fn is_empty(&self) -> bool {
-        self.pallet.is_empty()
-    }
-
-    pub(crate) fn owner(&self) -> Option<Player> {
-        self.pallet.apex()
-    }
-
-    pub(crate) fn stack(&self, player: &Player) -> Result<Self> {
-        let stacked = self.pallet.stack(player)?;
-        Ok(Self {
-            position: self.position.clone(),
-            pallet: stacked,
-        })
-    }
-
-    pub(crate) fn unstack(&self) -> Result<Self> {
-        let unstacked = self.pallet.unstack()?;
-        Ok(Self {
-            position: self.position.clone(),
-            pallet: unstacked,
-        })
     }
 }
 
 #[cfg(test)]
-mod testing {
-    use crate::{
-        player::Player,
-        position::{Column, Position, Row},
-    };
-
-    pub(crate) fn spawn_player() -> Player {
-        Player::new()
-    }
-
-    pub(crate) fn spawn_position() -> Position {
-        Position::new(Column::LeftEdge, Row::Top)
-    }
+fn spawn_player() -> Player {
+    Player::new()
 }
 
 #[test]
 fn new_occupied() {
-    let position = testing::spawn_position();
-    let player = testing::spawn_player();
-    let cell = Cell::new_occupied(&position, &player);
+    let player = spawn_player();
+    let cell = Cell::new_occupied(player.clone());
     assert_eq!(
         cell,
-        Cell {
-            position,
-            pallet: Pallet {
-                players: [Some(player.clone()), None, None]
-            },
-        }
+        Cell { pallet:  [Some(player.clone()), None, None] },
     );
     assert_eq!(cell.owner(), Some(player.clone()));
-    assert_eq!(cell.pallet.height(), 1);
+    assert_eq!(cell.height(), 1);
     assert!(!cell.is_empty());
 }
 
 #[test]
 fn new_empty() {
-    let position = testing::spawn_position();
-    let cell = Cell::new_empty(&position);
+    let cell = Cell::new_empty();
     assert_eq!(
         cell,
         Cell {
-            position,
-            pallet: Pallet {
-                players: [None; PALLET_HEIGHT_LIMIT]
-            },
-        }
+            pallet: [None; PALLET_HEIGHT_LIMIT],
+        },
     );
     assert!(cell.is_empty());
     assert_eq!(cell.owner(), None);
@@ -164,9 +97,8 @@ fn new_empty() {
 
 #[test]
 fn stack() {
-    let position = testing::spawn_position();
-    let player_1 = testing::spawn_player();
-    let cell = Cell::new_empty(&position);
+    let player_1 = spawn_player();
+    let cell = Cell::new_empty();
     let first_stacked = cell.stack(&player_1);
     assert!(first_stacked.is_ok());
     let cell_has_one_player = first_stacked.unwrap();
@@ -175,37 +107,31 @@ fn stack() {
         over_stacking_cell.stack(&player_1),
         Err(Error::AlreadyOccupied(player_1.clone())),
     );
-    assert_eq!(cell_has_one_player.pallet.height(), 1);
+    assert_eq!(cell_has_one_player.height(), 1);
     assert_eq!(cell_has_one_player.owner(), Some(player_1.clone()));
-    let player_2 = testing::spawn_player();
+    let player_2 = spawn_player();
     let second_stacked = cell_has_one_player.stack(&player_2);
     assert!(second_stacked.is_ok());
     let cell_has_two_players = second_stacked.unwrap();
     assert_eq!(
-        &cell_has_two_players.pallet.players,
+        &cell_has_two_players.pallet,
         &[Some(player_1.clone()), Some(player_2.clone()), None]
     );
     let stacking_error = cell_has_two_players
-        .stack(&testing::spawn_player())
+        .stack(&spawn_player())
         .unwrap()
-        .stack(&testing::spawn_player());
+        .stack(&spawn_player());
     assert_eq!(stacking_error, Err(Error::ReachedPalletHeightLimit));
 }
 
 #[test]
 fn unstack() {
-    let position = testing::spawn_position();
-    let player_1 = testing::spawn_player();
-    let cell = Cell::new_occupied(&position, &player_1);
+    let player_1 = spawn_player();
+    let cell = Cell::new_occupied(player_1.clone());
     let unstacked = cell.unstack();
     assert_eq!(
         unstacked,
-        Ok(Cell {
-            position: position.clone(),
-            pallet: Pallet {
-                players: [None; PALLET_HEIGHT_LIMIT]
-            },
-        })
+        Ok(Cell { pallet: [None; PALLET_HEIGHT_LIMIT] }),
     );
     let empty_cell = unstacked.unwrap();
     let cannot_unstack = empty_cell.unstack();
